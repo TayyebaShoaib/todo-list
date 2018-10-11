dce55418c0d6985de943235c39c5baaf1c9fe856
Tayyeba Shoaib <tayyeba.t@gmail.com>
2018-10-11 18:52:15 +0100

    version 2: Working with functions

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 /**
- * V1 requirements
- * It should have a place to store todos
- * It should have a way to display todos
- * It should have a way to add new todos
- * It should have a way to change a todo
- * It should have a way to delete a todo
+ * Version 2 Requirements
+ * It should have a function to display todos
+ * It should have a function to add todos
+ * It should have a function to change todos
+ * It should have a function to delete todos
  **/
 
-// It should have a place to store todos
-var todos = ["item 1", "item 2", "item 3"];
+var todos = ["item 1", "item 2", "item 3", "item 4"];
 
-// It should have a way to display todos
-console.log("My todos:", todos);
+// It should have a function to display todos
+function displayTodos()
+{
+    console.log("My todos:", todos);
+}
 
-// It should have a way to add new todos
-todos.push("item 4", "item 5");
+// It should have a function to add todos
+function addTodo(todo) 
+{
+    todos.push(todo);
+    displayTodos();
+}
 
-// It should have a way to change a todo
-todos[0] = "item 1 updated";
+// It should have a function to change todos
+function changeTodo(position, newItem) 
+{
+    todos[position] = newItem;
+    displayTodos();
+}
 
-// It should have a way to delete a todo
-todos.splice(0, 1);
+// It should have a function to delete todos
+function deleteTodos(position) 
+{
+    todos.splice(position, 1);
+    displayTodos();
+}
